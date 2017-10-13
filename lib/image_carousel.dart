dd00976f9edab0995e2a51bef4626ac6ec829bab
Theo Bouwman <theobouwman98@gmail.com>
2017-10-13 23:19:58 +0200

    platform specific

--- lib/image_carousel.dart
+++ lib/image_carousel.dart
@@ -1,20 +1,24 @@
 library image_carousel;
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class ImageCarousel extends StatefulWidget {
   final List<CarouselImage> images;
   final double height;
+  final TargetPlatform platform;
 
-  ImageCarousel(this.images, {this.height = 200.0});
+  // Images will shrink according to the value of [height]
+  // If you prefer to use the Material or Cupertino style activity indicator set the [platform] parameter
+  ImageCarousel(this.images, {this.height = 250.0, this.platform});
 
   @override
   State createState() => new _ImageCarouselState();
 }
 
-class _ImageCarouselState extends State<ImageCarousel>
-    with SingleTickerProviderStateMixin {
+class _ImageCarouselState extends State<ImageCarousel> with SingleTickerProviderStateMixin {
   TabController _tabController;
 
   @override
@@ -36,36 +40,72 @@ class _ImageCarouselState extends State<ImageCarousel>
       height: widget.height,
       child: new TabBarView(
         controller: _tabController,
-        children: widget.images,
+        children: widget.images.map((CarouselImage ci) {
+          return new CarouselImageWidget(widget, ci);
+        }).toList(),
       ),
     );
   }
 }
 
-class CarouselImage extends StatelessWidget {
-  final String title;
+class CarouselImage {
   final ImageType type;
   final String uri;
 
-  CarouselImage(this.type, this.uri, {this.title});
+  CarouselImage(this.type, this.uri);
+}
 
-  ImageProvider _getImage() {
-    switch (type) {
-      case ImageType.asset:
-        return new AssetImage(uri);
+class CarouselImageWidget extends StatefulWidget {
+  final ImageCarousel carousel;
+  final CarouselImage carouselImage;
+
+  CarouselImageWidget(this.carousel, this.carouselImage);
+
+  Image getImage() {
+    switch (carouselImage.type) {
       case ImageType.network:
-        return new NetworkImage(uri);
+        return new Image.network(carouselImage.uri);
       default:
-        return new AssetImage(uri);
+        return new Image.asset(carouselImage.uri);
+    }
+  }
+
+  @override
+  State createState() => new _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImageWidget> {
+  Image _image;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _image = widget.getImage();
+
+    if (widget.carouselImage.type == ImageType.network) {
+      _image.image.resolve(new ImageConfiguration()).addListener((i, b) {
+        if (mounted) {
+          setState(() {
+            _loading = false;
+          });
+        }
+      });
+    } else {
+      setState(() {
+        _loading = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new DecoratedBox(
-      decoration: new BoxDecoration(
-        image: new DecorationImage(image: _getImage()),
-      ),
+    return new Center(
+      child: _loading
+          ? widget.carousel.platform == TargetPlatform.android
+              ? new CircularProgressIndicator()
+              : new CupertinoActivityIndicator()
+          : _image,
     );
   }
 }
--- test/image_carousel_test.dart
+++ test/image_carousel_test.dart
@@ -3,5 +3,5 @@ import 'package:test/test.dart';
 import 'package:image_carousel/image_carousel.dart';
 
 void main() {
-  // TODO: write tests
+  // TODO: write test
 }
